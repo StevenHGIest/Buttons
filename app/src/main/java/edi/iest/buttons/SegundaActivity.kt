3264dbb2cfe85410d0cdc30af97410e338a6d571
StevenHGIest <steven.horstman@iest.edu.mx>
2023-01-30 18:55:20 -0600

    fourth

--- app/src/main/java/edi/iest/buttons/SegundaActivity.kt
+++ app/src/main/java/edi/iest/buttons/SegundaActivity.kt
@@ -5,12 +5,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import edi.iest.buttons.R.*
 
-class SegundaActivity : AppCompatActivity() {
+class SegundaActivity : AppCompatActivity(), View.OnClickListener {
 
-
+    private var cambioIcono = false
+    var bnPantalla = findViewById<Button>(R.id.bnPantalla)
+    var ivLogo = findViewById<ImageView>(R.id.ivLogo)
+    var bnCerrar = findViewById<Button>(R.id.bnCerrar)
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,15 +28,50 @@ class SegundaActivity : AppCompatActivity() {
         Log.d("Datos", "Datos recividos $fraseRecivida y $edad")
 
 
-        var btn = findViewById<Button>(R.id.bnPantalla)
-        btn.setOnClickListener{
-            val i = Intent(this, MainActivity::class.java)
-            startActivity(i)
+        bnCerrar.setOnClickListener(this)
+
+    /*btn.setOnClickListener{
+
         }
 
-        var bnCerrar = findViewById<Button>(R.id.bnCerrar)
+
         bnCerrar.setOnClickListener {
             finish()
+        }
+        // le damos override on clicl sitener, le damos ok
+
+        ivLogo.setOnClickListener {
+            var arregloImagenes = arrayOf<Int>(R.drawable.anahuac, R.drawable.iest2)
+            if (cambioIcono) {
+                ivLogo.setImageResource(arregloImagenes[0])
+            }else {
+                ivLogo.setImageResource(arregloImagenes[1])
+            }
+
+            cambioIcono = !cambioIcono
+            // setImageRecource cambi ala imagen de un iamageView
+            //ivLogo.setImageResource(R.drawable.anahuac)        }
+
+        }*/
+    }
+    //De aqui ocupara todos los metodos
+    override fun onClick(p0: View?) {
+        when(p0?.id){
+            R.id.bnCerrar -> finish()
+            R.id.bnPantalla -> {
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i)
+            }
+            R.id.ivLogo -> {
+                var arregloImagenes = arrayOf<Int>(R.drawable.anahuac, R.drawable.iest2)
+                if (cambioIcono) {
+                    ivLogo.setImageResource(arregloImagenes[0])
+                }else {
+                    ivLogo.setImageResource(arregloImagenes[1])
+                }
+
+                cambioIcono = !cambioIcono
+            }
         }
     }
 }
